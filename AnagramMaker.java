ca261e9583d7549a3fea66a4c2bfd1fcb284c834
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2018-08-02 16:18:47 +0100

    AnagramMaker

---
+++ AnagramMaker.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class AnagramMaker {
+
+    /**
+	 *	makeAnagram takes two input strings and returns the number of characters that must be deleted so a & b
+	 *	become anagrams
+	 *	@param String a, b - the two strings to be pared down so they become anagrams of each other
+	 *	@return int requiredDeletions - the amount of characters in each string that would be deleted so a & b 
+	 * 	become anagrams of each other
+	 */
+    static int makeAnagram(String a, String b) {
+        int charCountA[] = new int[26]; //count occurances of A-Z in a
+        int charCountB[] = new int[26]; //count occurances of A-Z in b
+        /**
+         * Loop through the input strings and increment the corresponding elements in
+         * charCountA & B
+         */
+        for(int i = 0; i < a.length(); i++){
+            charCountA[a.charAt(i) -'a']++;
+        }
+        for(int i = 0; i < b.length(); i++){
+            charCountB[b.charAt(i) -'a']++;
+        }
+        int requiredDeletions = 0;
+        //Loop through charCountA & B 
+        for(int i = 0; i < 26; i++){
+            requiredDeletions += Math.abs(charCountA[i]-charCountB[i]);
+        }
+        return requiredDeletions;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String a = scanner.nextLine();
+
+        String b = scanner.nextLine();
+
+        int res = makeAnagram(a, b);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
--- leftrotation.java
+++ leftrotation.java
@@ -11,12 +11,14 @@ public class Solution {
         int n = in.nextInt(); //The number of elements in the array
         int k = in.nextInt(); //The number of left rotations to be done
         int a[] = new int[n];
-        for(int a_i=0; a_i < n; a_i++){
-            a[a_i] = in.nextInt();
+        for(int i=0; i < n; i++){
+            a[i] = in.nextInt();
         }
-        for(int bI = 0; bI < n; bI++){
-            int newPos = (bI + n + k) % n; //Add the array's length to bI to avoid a negative result, then after adding the amount of left rotations to be done, modulo the result by the array's length
-            int temp = a[newPos];
+        for(int j = 0; j < n; j++){
+			/* Add the array's length to bI to avoid a negative result, 
+			then after adding the amount of left rotations to be done, 
+			modulo the result by the array's length */
+            int newPos = (j + n + k) % n; 
             System.out.print(a[newPos] + " ");
         }
     }
