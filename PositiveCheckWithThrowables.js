5c6e77b902133342bfa4a4629658bebce9342195
Hakosuka <ciarancumiskey18594@gmail.com>
2020-02-11 11:00:13 +0000

    More Hackerrank

---
+++ PositiveCheckWithThrowables.js
@@ -0,0 +1,50 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Returns "YES" if a is positive, otherwise throws an error depending on whether it's 0 or negative.
+ */
+function isPositive(a) {
+    if (a < 0){
+        throw Error("Negative Error");
+    } else if (a === 0){
+        throw Error("Zero Error");
+    }
+    return "YES"
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    for (let i = 0; i < n; i++) {
+        const a = +(readLine());
+      
+        try {
+            console.log(isPositive(a));
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+}
