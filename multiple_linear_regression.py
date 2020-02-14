c0ef0a19cafecbb70526fd7c696e533449192371
Hakosuka <ciarancumiskey18594@gmail.com>
2020-02-14 17:22:13 +0000

    More Hackerrank stuff

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -16,21 +16,18 @@ set_X = numpy.array(set_X, float)
 set_Y = numpy.array(set_Y, float)
 X_new = numpy.array(X_new, float)
 
-#center
 X_R = set_X-numpy.mean(set_X, axis=0)
 Y_R = set_Y-numpy.mean(set_Y)
-
 #calculate beta using matrix operations
 beta = numpy.dot(
     numpy.linalg.inv(
         numpy.dot(X_R.T,X_R)),
         numpy.dot(X_R.T,Y_R))
 
-#predict
+#predict Y values for the additional feature sets
 X_new_R = X_new-numpy.mean(set_X,axis=0)
 Y_new_R = numpy.dot(X_new_R,beta)
 Y_new = Y_new_R + numpy.mean(set_Y)
 
-#print
 for i in Y_new:
     print(round(float(i),2))
