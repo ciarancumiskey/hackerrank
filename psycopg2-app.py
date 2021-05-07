257a86a7a59b6472040f46497888d71ffdba144a
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-05-07 18:27:25 +0100

    Added comments and iteration
    Completed Exercise 3 of Udacity's "Interacting With Databases" lesson in their Full-Stack Web Dev course.

--- psycopg2-app.py
+++ psycopg2-app.py
@@ -2,15 +2,18 @@ import psycopg2
 
 print("Connecting to database")
 conn = psycopg2.connect(dbname="db1", user="postgres", password="AsIfIdLeaveThatHere", host="127.0.0.1", port="5432")
+
 cur = conn.cursor()
+#Remove any previous version of table1, then create a new one
 cur.execute("DROP TABLE IF EXISTS table1;")
 cur.execute('''CREATE TABLE table1 
     (id serial PRIMARY KEY, 
     num integer, 
     data varchar);
 ''')
-cur.execute("INSERT INTO table1 (num, data) VALUES (%s, %s)", (44, "#BLESSED"))
 
+#Populate table1 with a basic SQL query and then some string interpolation
+cur.execute("INSERT INTO table1 (num, data) VALUES (%s, %s)", (44, "#BLESSED"))
 SQL = 'INSERT INTO table1 (num, data) VALUES (%(num)s, %(data)s);'
 data = {
     'num': 77,
@@ -23,11 +26,11 @@ data = {
     'data': 'MAX POWER'
 }
 cur.execute(SQL, data)
+#Return the results from the populated table
 cur.execute("SELECT * FROM table1;")
-result = cur.fetchmany(2)
-print(result)
-result = cur.fetchone()
-print("fetchone(): ", result)
+for record in cur:
+    print(record)
+#Commit the transaction and then close the connection to the database
 conn.commit()
 cur.close()
 conn.close()
